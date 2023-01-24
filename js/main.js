5d1a6939b5a67d140ab5a2c00b2a0a7e7a1cb2fc
ferfar92 <ffarias934@gmail.com>
2023-01-24 12:20:05 -0600

    Se implementó el botón de copiar texto

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-
+// Inicializamos variables
 let resultado;
 let botonEncriptar;
 let mensaje;
 let mensajeM;
+//Obtener os objetos de la acción del botón
 botonEncriptar = document.getElementById("encriptar");
 botonEncriptar.addEventListener("click", encriptarM);
 
 botonDesencriptar = document.getElementById("desencriptar");
 botonDesencriptar.addEventListener("click",desencriptar);
 
-
-
+//Función de encriptar
 function encriptarM(mensaje){
     mensaje = document.getElementById("mensaje");
     resultado = document.getElementById("resultado");
@@ -18,20 +18,32 @@ function encriptarM(mensaje){
     let mensaje1 = mensaje.value;
     let mensajeM = minusculas(mensaje1);
 
+    //Compraración y remplazo con .replace
     mensajeM = mensajeM.replace(/e/g,'enter');
     mensajeM = mensajeM.replace(/i/g,'imes');
     mensajeM = mensajeM.replace(/a/g,'ai');
     mensajeM = mensajeM.replace(/o/g,'ober');
     mensajeM = mensajeM.replace(/u/g,'ufat');
 
+    //Insercción del nuevo mensaje en HTML
     mensaje.value = "";
     resultado.innerHTML= `
     <h2 class= "txtEncabezado"><strong>Texto Encriptado: </strong></h2>
     <div class="txtEncriptado">${mensajeM}</div>
     <input type="submit" value="Copiar" class="copiar" id="copiar"> `;
 
+    //Función copiar texto en el portapapeles
+    if(mensajeM != ""){
+        let copiar = document.getElementById("copiar");
+        copiar.addEventListener("click", function(e){
+            navigator.clipboard.writeText(mensajeM);
+            alert("Se copió el texto: " + mensajeM);
+        });
+    }
+
 }
 
+//Función de desencriptar
 function desencriptar(mensaje){
     mensaje = document.getElementById("mensaje");
     resultado = document.getElementById("resultado");
@@ -39,34 +51,31 @@ function desencriptar(mensaje){
     let mensaje1 = mensaje.value;
     let mensajeM = minusculas(mensaje1);
 
+    //Compraración y remplazo con .replace
     mensajeM = mensajeM.replace(/enter/g,'e');
     mensajeM = mensajeM.replace(/imes/g,'i');
     mensajeM = mensajeM.replace(/ai/g,'a');
     mensajeM = mensajeM.replace(/ober/g,'o');
     mensajeM = mensajeM.replace(/ufat/g,'u');
 
+    //Insercción del nuevo mensaje en HTML
     mensaje.value = "";
     resultado.innerHTML= `
     <h2 class= "txtEncabezado"><strong>Texto Desencriptato: </strong></h2>
     <div class="txtEncriptado" id="mensajeD">${mensajeM}</div>
     <input type="submit" value="Copiar" class="copiar" id="copiar">`;
 
-    if(mensajeM != null){
+    //Función copiar texto en el portapapeles
+    if(mensajeM != ""){
         let copiar = document.getElementById("copiar");
         copiar.addEventListener("click", function(e){
-            let copytext = document.getElementById("mensajeD");
-            copytext.focus();
-            copytext.setSelectionRange(0, 999999); // For mobile devices
-          
-             // Copy the text inside the text field
-            navigator.clipboard.writeText(copytext.value);
-          
-            // Alert the copied text
-            alert("Copied the text: " + copytext.value);
+            navigator.clipboard.writeText(mensajeM);
+            alert("Se copió el texto: " + mensajeM);
         });
     }
 }
 
+//Función para convertir en minúsculas el texto
 function minusculas(mensaje1){
     return mensaje1.toLowerCase();
 }
